ca56072fb371f5f21aca893be444de7b1f24395e
Brooke G <brooke.a.guarienti@gmail.com>
2020-02-21 23:20:05 -0600

    updated javascript and html ids to match

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// Create variables that hold references to the places in the HTML where we want to display things.
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guesses-left-text");
+var guessesMadeText = document.getElementById("guesses-made-text");
